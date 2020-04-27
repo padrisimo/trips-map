683ff28572e54212445176a3db79e2d4b2f17b13
padrisimo <chamizho@gmail.com>
2020-04-27 22:12:40 +0200

    Updates

---
+++ precache-manifest.b6f988805c746d7935288f43df0e282f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2421c21bfc1f08889d9f33f90f0bf9",
+    "url": "/trips-map/index.html"
+  },
+  {
+    "revision": "9222e7e541226d65423c",
+    "url": "/trips-map/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "9222e7e541226d65423c",
+    "url": "/trips-map/static/js/2.1d8512ec.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/trips-map/static/js/2.1d8512ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5788b0302689dea123b8",
+    "url": "/trips-map/static/js/main.fb16757e.chunk.js"
+  },
+  {
+    "revision": "a84e2d0944a60ef0c79d",
+    "url": "/trips-map/static/js/runtime-main.9cc28940.js"
+  }
+]);
