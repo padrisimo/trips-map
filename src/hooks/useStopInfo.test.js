895ff748dc05955456ba0f558b538194456d8025
padrisimo <chamizho@gmail.com>
2020-06-08 16:08:55 +0200

    useStopInfo testdone

--- src/hooks/useStopInfo.test.js
+++ src/hooks/useStopInfo.test.js
@@ -1,12 +1,12 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import useStopInfo from 'hooks/useStopInfo';
 import api from 'api/trips';
-
-api.get = jest.fn();
+import { STOP_FAIL } from 'constants/vars';
 
 describe('useStopInfo', () => {
   it('fethes stop info from api with query', async () => {
     const { result } = renderHook(useStopInfo);
+    api.get = jest.fn();
 
     await act(async () => {
       result.current.getStopInfo(1);
@@ -16,9 +16,40 @@ describe('useStopInfo', () => {
   });
 
   describe('when got data succesfully', () => {
-    it.todo('handles succesful state correctly');
+    it('handles succesful state correctly', async () => {
+      api.get = jest.fn(() => ({
+        data: {
+          stopName: 'Arc de Triomf'
+        }
+      }));
+
+      const { result } = renderHook(useStopInfo);
+
+      await act(async () => {
+        result.current.getStopInfo(1);
+      });
+
+      expect(result.current.result).toStrictEqual({
+        stopName: 'Arc de Triomf'
+      });
+    });
   });
+
   describe('whit an error during request', () => {
-    it.todo('handles error state corecctly');
+    it('handles error state corecctly', async () => {
+      const { result } = renderHook(useStopInfo);
+
+      api.get = jest.fn(() => {
+        return new Promise(() => {
+          throw 'Error';
+        });
+      });
+
+      await act(async () => {
+        result.current.getStopInfo(1);
+      });
+
+      expect(result.current.errorMessage).toStrictEqual(STOP_FAIL);
+    });
   });
 });
